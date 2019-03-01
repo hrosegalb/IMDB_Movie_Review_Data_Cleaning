aecdeaafb22673a61d05bfd32167947677022fe0
Hannah Galbraith <hannahrosegalbraith@gmail.com>
2019-03-01 15:32:29 -0800

    Finished program

--- csv_creation.py
+++ csv_creation.py
@@ -1,5 +1,39 @@
 import pandas as pd
 
+def create_directed_by_csv(dataframe):
+    directed_by_df = dataframe[['director_name', 'movie_title']]
+    directed_by_df = directed_by_df.rename(columns={'director_name': 'name'})
+    return directed_by_df
+
+
+def create_acted_in_csv(dataframe):
+    acted_in_df = pd.DataFrame(columns=['name', 'movie_title'])
+
+    num_rows = dataframe.shape[0]
+    k = 0
+    for i in range(num_rows):
+        row = dataframe.iloc[i]
+        movie_title = row['movie_title']
+        movie_title = movie_title.rstrip()
+        
+        actor1 = row['actor_1_name']
+        actor1 = actor1.rstrip()
+        acted_in_df.loc[k] = [actor1, movie_title]
+        k += 1
+
+        actor2 = row['actor_2_name']
+        actor2 = actor2.rstrip()
+        acted_in_df.loc[k] = [actor2, movie_title]
+        k += 1
+
+        actor3 = row['actor_3_name']
+        actor3 = actor3.rstrip()
+        acted_in_df.loc[k] = [actor3, movie_title]
+        k += 1
+
+    return acted_in_df
+
+
 def create_genre_csv(dataframe):
     genre_df = pd.DataFrame(columns=['movie_title', 'genre'])
     
@@ -21,8 +55,27 @@ def create_genre_csv(dataframe):
 
 def create_director_csv(dataframe):
     director_df = dataframe[['director_name', 'director_facebook_likes']]
-    director_df.drop_duplicates(subset='director_name', keep="first")
+    director_df = director_df.drop_duplicates(subset='director_name', keep="first")
+    director_df = director_df.rename(columns={'director_name': 'name', 'director_facebook_likes': 'numfacebooklikes'})
     return director_df
+
+
+def create_score_csv(dataframe):
+    score_df = dataframe[['movie_title', 'num_critic_for_reviews', 'imdb_score']]
+    score_df = score_df.rename(columns={'num_critic_for_reviews': 'numreviews'})
+    score_df['movie_title'] = score_df['movie_title'].str.strip()
+    return score_df
+
+
+def create_movie_csv(dataframe):
+    movie_df = dataframe[['movie_title', 'title_year', 'duration',
+                          'budget', 'gross', 'content_rating',
+                          'language', 'country', 'movie_facebook_likes']]
+    movie_df = movie_df.astype({'title_year': int})
+    movie_df = movie_df.rename(columns={'movie_title': 'title', 'title_year': 'year', 
+                                        'content_rating': 'rating', 'movie_facebook_likes': 'numfacebooklikes'})
+    movie_df['title'] = movie_df['title'].str.strip()
+    return movie_df
 
 
 def read_in_csv():
@@ -31,6 +84,9 @@ def read_in_csv():
     df = df[bool_series]
 
     bool_series = pd.notnull(df['movie_title'])
+    df = df[bool_series]
+
+    bool_series = pd.notnull(df['title_year'])
     df = df[bool_series]
 
     bool_series = pd.notnull(df['actor_1_name'])
@@ -69,10 +125,28 @@ def create_actor_csv(dataframe):
         actor_df.loc[k] = [actor3, actor3_likes]
         k += 1
 
-    actor_df.drop_duplicates(subset='actor_name', keep="first")
+    actor_df = actor_df.drop_duplicates(subset='actor_name', keep="first")
     return actor_df
 
 df = read_in_csv()
+
 #director_df = create_director_csv(df)
+#director_csv = director_df.to_csv (r'directors.csv', index=False, header=True)
+
 #genre_df = create_genre_csv(df)
-actor_df = create_actor_csv(df)
+#genre_csv = genre_df.to_csv (r'genres.csv', index=False, header=True)
+
+#actor_df = create_actor_csv(df)
+#actor_csv = actor_df.to_csv(r'actors.csv', index=False, header=True)
+
+#acted_in_df = create_acted_in_csv(df)
+#acted_in_csv = acted_in_df.to_csv(r'acted_in.csv', index=False, header=True)
+
+#directed_by_df = create_directed_by_csv(df)
+#directed_by_csv = directed_by_df.to_csv(r'directed_by.csv', index=False, header=True)
+
+#score_df = create_score_csv(df)
+#score_csv = score_df.to_csv(r'scores.csv', index=False, header=True)
+
+movie_df = create_movie_csv(df)
+movie_csv = movie_df.to_csv(r'movies.csv', index=False, header=True)
